69ee9dafe5458d3b2e2c9e308177fab3df3eb8ca
PeterNdomano <peterndomano@gmail.com>
2022-10-07 10:07:38 +0300

    Meta Scrapper

--- Meta-Scrapper/main.py
+++ Meta-Scrapper/main.py
@@ -13,14 +13,53 @@ import requests
 
 def main(argv):
     cmd_args = argv[1:];
+    url = ""
+    dir = ""
+    if(len(cmd_args) >= 2):
+        opts, args = getopt.getopt(cmd_args, "d:u:", ["url=", "dir"])
+        for opt, val in opts:
+            if opt in ("-d", '--dir'):
+                dir = val
+            elif opt in ("-u", '--url'):
+                url = val
 
+        if(len(url) > 0 and len(dir) > 0):
+            print("Meta Scrapper is starting......")
+            getRobotsTxt(url)
+            getSitemapXml(url)
+            getSecurityTxt(url)
+            getHumansTxt(url)
+            scanForMetaTags(url)
+            scanWellKnowsDir(url)
+        else:
+            showHelp(argv);
+    else:
+        showHelp(argv)
+
+def getRobotsTxt(url):
+    print('Fetching robots.txt...')
+
+def getSitemapXml(url):
+    print('Fetching sitemap.xml...');
+
+def getSecurityTxt(url):
+    print('Fetching security.txt....')
+
+def getHumansTxt(url):
+    print('Fetching humans.txt....')
+
+def scanForMetaTags(url):
+    print('Scanning meta tags')
+
+def scanWellKnowsDir(url):
+    print('Scanning .well-known folder...')
+    
 def scanResponse(response):
     print("SCAN OUTPUT:")
     print(response)
 
 def showHelp(argv):
-    help_text = "Use -u <targetUrl> and -d <outputDirectory> to specify targetUrl and directory where metadata and metafiles will be saved.\n Also make sure you're connected to the internet"
-    help_text = "{0} "+help_text.format(argv[0]);
+    help_text = "{0} Use -u <targetUrl> and -d <outputDirectory> to specify targetUrl and directory where metadata and metafiles will be saved.\n Also make sure you're connected to the internet".format(argv[0]);
     print(help_text);
 
 
